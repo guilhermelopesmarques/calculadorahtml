d121d546546d69d4d5a5115254da7e1f737e12a0
guilherme <112526329+guilhermelopesmarques@users.noreply.github.com>
2023-07-12 22:13:19 -0300

    t

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-function valores(valor){
+function Valor(valores){
 
     resultado = document.getElementById('resultado').innerHTML
-    document.getElementById('resultado').innerHTML += valor
+    document.getElementById('resultado').innerHTML += valores
 }
 
-function operadores(operedor){
-
+function operadores(operador){
+calculo = operador;
+valor1 = document.getElementById('resultado').innerHTML
+document.getElementById('resultado').innerHTML = ""
     
+}
+
+function Calcular(){
+    valor2 = document.getElementById('resultado').innerHTML;
+if(calculo == '+'){
+    total = parseInt(valor1) + parseInt(valor2);
+} else if (calculo == '-'){
+    total = parseInt(valor1) - parseInt(valor2);
+}  else if (calculo == '*'){
+    total = parseInt(valor1) * parseInt(valor2);
+}  else if (calculo == '/'){
+    total = parseInt(valor1) / parseInt(valor2);
+}
+
+    document.getElementById('resultado').innerHTML = total;
+}
+
+function limpar(){
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function limpa(){
+    let resultado = document.getElementById('resultado').innerHTML;
+  let novoResultado = resultado.slice(0, -1);
+  document.getElementById('resultado').innerHTML = novoResultado;
 }
